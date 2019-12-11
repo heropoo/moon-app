04006edac598114cc62765c0f6b386538d4720dc
heropoo <aiyouyou1000@163.com>
2019-12-12 00:20:34 +0800

    config

--- config/route.php
+++ config/route.php
@@ -24,28 +24,3 @@ return [
         ],
     ]
 ];
-/**
- * Date: 2019-11-05
- * Time: 18:40
- */
-$routes_path = dirname(__DIR__) . '/routes';
-return [
-    'namespace' => "App\\Controllers",
-    'middleware' => [],
-    'groups' => [
-        'web' => [
-            'file' => $routes_path . '/web.php',
-//'namespace' => '',
-//'prefix' => '',
-            'middleware' => [
-                'App\Middleware\SessionStart',
-            ]
-        ],
-        'api' => [
-            'file' => $routes_path . '/api.php',
-            'namespace' => 'Api',
-            'prefix' => 'api',
-            'middleware' => [],
-        ],
-    ]
-];
