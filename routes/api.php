560bddb16da7c270726e63bf9fb893afc2b4d5d3
heropoo <aiyouyou1000@163.com>
2019-12-12 11:51:51 +0800

    test

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,10 @@
 <?php
+/**
+ * @var \Moon\Routing\Router $router
+ */
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+$router->get('{aid}/user/list', function ($aid) {
+    return new JsonResponse(['code' => 0, 'msg' => 'ok', 'data' => ['aid' => $aid]]);
+});
