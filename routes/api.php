2bad6c4c95dacd3f3365105f42de801a80f482f7
heropoo <aiyouyou1000@163.com>
2019-12-27 16:26:32 +0800

    test

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-$router->get('{version:v\d+}/{aid:\d+}/user/list', function ($version, $aid) {
+$router->get('{version:v\d+(?:\.\d+)*}/{aid:\d+}/user/list', function ($version, $aid) {
+//$router->get('{version:v\d+}/{aid:\d+}/user/list', function ($version, $aid) {
     return new JsonResponse(['code' => 0, 'msg' => 'ok', 'data' => ['version' => $version, 'aid' => $aid]]);
 })->name('user.list');
