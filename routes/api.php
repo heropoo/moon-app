141094d3a10a1f8e86a3a7d58803e82d0e0126da
heropoo <aiyouyou1000@163.com>
2020-01-06 18:54:52 +0800

    test

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,10 @@
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+$router->get('/test', function () {
+    return ['code' => 0, 'msg' => 'api test'];
+});
+
 $router->get('{version:v\d+(?:\.\d+)*}/{aid:\d+}/user/list', function ($version, $aid) {
 //$router->get('{version:v\d+}/{aid:\d+}/user/list', function ($version, $aid) {
     return new JsonResponse(['code' => 0, 'msg' => 'ok', 'data' => ['version' => $version, 'aid' => $aid]]);
