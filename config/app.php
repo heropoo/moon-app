ff6e42ab59427ab61c8e28b530047c313e3886dc
heropoo <aiyouyou1000@163.com>
2020-01-07 18:17:59 +0800

    config

--- config/app.php
+++ config/app.php
@@ -18,8 +18,8 @@ return [
             'host' => env('REDIS_HOST', 'localhost'),
             'port' => env('REDIS_PORT', 6379),
             'password' => env('REDIS_PASSWORD'),
-            'db' => env('REDIS_DATABASE', 0)
+            'database' => env('REDIS_DATABASE', 0)
         ]
     ],
-    'bootstrap' => ['db', 'redis']
+    'bootstrap' => ['redis']
 ];
--- config/db.php
+++ config/db.php
@@ -8,14 +8,17 @@
 
 return [
     'class' => 'Moon\Db\Connection',
-    'dsn' => 'mysql:host=' . env('DB_HOST', 'localhost') . ';dbname=' . env('DB_NAME', 'test') . ';port='.env('DB_PORT', '3306'),
-    'username' => env('DB_USER', 'root'),
-    'password' => env('DB_PWD', ''),
-    'charset' => 'utf8mb4',
-    'tablePrefix' => 'tt_',
-    'emulatePrepares' => false,
-    'options' => [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        //PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
+    'master' => [
+        'dsn' => 'mysql:host=' . env('DB_HOST', 'localhost')
+            . ';dbname=' . env('DB_NAME', 'test') . ';port=' . env('DB_PORT', '3306'),
+        'username' => env('DB_USER', 'root'),
+        'password' => env('DB_PWD', ''),
+        'charset' => 'utf8mb4',
+        'tablePrefix' => 'tt_',
+        'emulatePrepares' => false,
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            //PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
+        ]
     ]
 ];
